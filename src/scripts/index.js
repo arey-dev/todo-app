0035c1252f42992c383564a4d158648bee7a8c49
R.A Borja <raborja.dev@gmail.com>
2022-10-10 20:24:13 +0800

    clear contents

--- src/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const name = 'James';
-
-const person = { first: name }; // \r\n
-
-console.log(person); // \r\n
-
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
