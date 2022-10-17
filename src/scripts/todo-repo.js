dde05d0deddd913fdce5cbabd4c2efe6d3e4a8aa
R.A Borja <raborja.dev@gmail.com>
2022-10-17 20:00:55 +0800

    add todo-repo

---
+++ src/scripts/todo-repo.js
@@ -0,0 +1,14 @@
+/**
+ * repository for todos and
+ * functions to manage it
+ */
+
+const todoRepo = {
+  repo: [],
+
+  addTodo: (obj) => {
+    this.repo.push(obj);
+  },
+};
+
+export default todoRepo;
