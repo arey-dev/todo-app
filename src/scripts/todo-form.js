1d5972e3c3af9e72e59cc8f7a1468f93b5cbdbfa
R.A Borja <raborja.dev@gmail.com>
2022-10-18 02:27:00 +0800

    add event and handler

--- src/scripts/todo-form.js
+++ src/scripts/todo-form.js
@@ -1,10 +1,25 @@
 /**
  * renders todo-form from template
  */
+import PubSub from "pubsub-js";
+import todo from "./todo";
+
+const createTask = () => {
+  const input = document.forms[0].elements['task-input'];
+
+  const task = todo(input.value);
+
+  // publish
+  PubSub.publish("addTodo", task);
+}
 
 const todoForm = (container) => {
   const template = document.getElementById("todo-form-template");
   const form = template.content.cloneNode(true);
+
+  const btn = form.querySelector('[role="button"]');
+
+  btn.onclick = createTask;
 
   container.append(form);
 };
