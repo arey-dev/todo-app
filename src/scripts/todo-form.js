013209455977fe0d813ce984d60f3971e24285dc
R.A Borja <raborja.dev@gmail.com>
2022-10-18 18:36:39 +0800

    add onkeydown event and little validation on formr

--- src/scripts/todo-form.js
+++ src/scripts/todo-form.js
@@ -7,11 +7,15 @@ import todo from "./todo";
 const createTask = () => {
   const input = document.forms[0].elements["task-input"];
 
-  // create todo object
-  const task = todo(input.value);
+  if (input.value) {
+    // create todo object
+    const task = todo(input.value);
 
-  // publish
-  PubSub.publish("addTodo", task);
+    input.value = "";
+
+    // publish
+    PubSub.publish("addTodo", task);
+  }
 };
 
 const todoForm = (container) => {
@@ -19,8 +23,17 @@ const todoForm = (container) => {
   const form = template.content.cloneNode(true);
 
   const btn = form.querySelector('[role="button"]');
+  const input = form.querySelector('input[name="task-input"]');
 
   btn.onclick = createTask;
+
+  input.onkeydown = (e) => {
+    if (e.key === "Enter") {
+      createTask();
+    }
+  };
+
+  // btn.onclick = createTask;
 
   container.append(form);
 };
