852cf3dd5ea064bef4d14f41400c899a54c60e20
R.A Borja <raborja.dev@gmail.com>
2022-10-18 18:37:26 +0800

    add subscribers

--- src/scripts/todo-list-view.js
+++ src/scripts/todo-list-view.js
@@ -2,12 +2,24 @@
  * renders the container
  * for list of todos
  */
+import PubSub from "pubsub-js";
+import todoList from "./todo-list";
+import todoView from "./todo-view";
 
 const todosView = (container) => {
   const template = document.getElementById("todo-list-template");
-  const listCont = template.content.cloneNode(true);
+  const listContainer = template.content.cloneNode(true);
 
-  container.append(listCont);
+  container.append(listContainer);
+
+  const addTodoBound = todoList.addTodo.bind(todoList);
+  PubSub.subscribe("addTodo", (msg, data) => {
+    addTodoBound(data);
+  });
+
+  PubSub.subscribe("addTodo", (msg, data) => {
+    todoView(data);
+  });
 };
 
 export default todosView;
