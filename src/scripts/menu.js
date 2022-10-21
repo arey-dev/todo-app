76ebf029ba6ba3c5a7f36ca56123bbe5ff29c19f
R.A Borja <raborja.dev@gmail.com>
2022-10-21 17:59:47 +0800

    add the changing of btns elemnt on event handler

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -6,12 +6,17 @@ const menuView = (container) => {
   const template = document.getElementById("menu-template");
   const content = template.content.cloneNode(true);
 
+  // use event delegation 
   const allBtn = content.querySelector('button[data-menu="all"]');
   const activeBtn = content.querySelector('button[data-menu="active"]');
   const completeBtn = content.querySelector('button[data-menu="completed"]');
 
   allBtn.onclick = () => {
     const all = document.querySelectorAll(".hidden");
+
+    activeBtn.classList.remove("btn-active");
+    completeBtn.classList.remove("btn-active");
+
     all.forEach((element) => {
       element.classList.remove("hidden");
     });
@@ -20,6 +25,10 @@ const menuView = (container) => {
   activeBtn.onclick = () => {
     const active = document.querySelectorAll(".active");
     const completed = document.querySelectorAll(".task-complete");
+
+    activeBtn.classList.add("btn-active");
+    allBtn.classList.remove("btn-active");
+    completeBtn.classList.remove("btn-active");
 
     active.forEach((element) => {
       element.classList.remove("hidden");
@@ -33,6 +42,10 @@ const menuView = (container) => {
   completeBtn.onclick = () => {
     const active = document.querySelectorAll(".active");
     const completed = document.querySelectorAll(".task-complete");
+
+    completeBtn.classList.add("btn-active");
+    allBtn.classList.remove("btn-active");
+    activeBtn.classList.remove("btn-active");
 
     active.forEach((element) => {
       element.classList.add("hidden");
