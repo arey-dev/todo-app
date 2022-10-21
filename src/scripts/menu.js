e15dc53f2097f903e0734711708be29f44cba56d
R.A Borja <raborja.dev@gmail.com>
2022-10-21 17:40:14 +0800

    add tab change logic to menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,12 +1,49 @@
 /**
- * renders menu 
+ * renders menu
  */
 
 const menuView = (container) => {
-  const template = document.getElementById('menu-template');
+  const template = document.getElementById("menu-template");
   const content = template.content.cloneNode(true);
 
+  const allBtn = content.querySelector('button[data-menu="all"]');
+  const activeBtn = content.querySelector('button[data-menu="active"]');
+  const completeBtn = content.querySelector('button[data-menu="completed"]');
+
+  allBtn.onclick = () => {
+    const all = document.querySelectorAll(".hidden");
+    all.forEach((element) => {
+      element.classList.remove("hidden");
+    });
+  };
+
+  activeBtn.onclick = () => {
+    const active = document.querySelectorAll(".active");
+    const completed = document.querySelectorAll(".task-complete");
+
+    active.forEach((element) => {
+      element.classList.remove("hidden");
+    });
+
+    completed.forEach((element) => {
+      element.classList.add("hidden");
+    });
+  };
+
+  completeBtn.onclick = () => {
+    const active = document.querySelectorAll(".active");
+    const completed = document.querySelectorAll(".task-complete");
+
+    active.forEach((element) => {
+      element.classList.add("hidden");
+    });
+
+    completed.forEach((element) => {
+      element.classList.remove("hidden");
+    });
+  };
+
   container.append(content);
-}
+};
 
 export default menuView;
