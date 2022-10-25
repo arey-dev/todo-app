4df7f0394dc29274957eabb0f965eeff94b9b32e
R.A Borja <raborja.dev@gmail.com>
2022-10-26 02:58:20 +0800

    fix theme change

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,22 @@
-import '../stylesheets/index.css';
+import "../stylesheets/index.css";
+
+import todoForm from "./form-view";
+import todoListView from "./todo-list-view";
+import subscribers from "./todo-list";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const mainContent = document.getElementById("main-content");
+  const themeIcon = document.querySelector(".theme-icon");
+
+  todoForm(mainContent);
+  todoListView(mainContent);
+
+  // todo-list subscribers;
+  subscribers();
+
+  themeIcon.onclick = () => {
+    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("bg-dark");
+    themeIcon.classList.toggle("theme-icon-dark")
+  }
+});
